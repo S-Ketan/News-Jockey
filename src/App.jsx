5919160a9f3b1ad6646c4f969fd9d2bbdf003319
Ketan Sharma <ketan10923@gmail.com>
2024-11-12 12:14:20 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+      <Navbar title="News Monkey" />
+      </>
+    )
+  }
+}
+
+export default App
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav className="p-4 w-full text-white bg-black">
+        <div className="w-full flex justify-between items-center mx-auto">
+          <div className="flex items-center space-x-4">
+            <a className="text-xl font-bold" href="/">
+              {this.props.title}
+            </a>
+            <a
+              className="nav-link active px-3 py-2 rounded block md:inline-block"
+              aria-current="page"
+              href="/"
+            >
+              Home
+            </a>
+            <a
+              className="nav-link px-3 py-2 rounded block md:inline-block"
+              href="/about"
+            >
+              About
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;

d5a61f355cc0ffbc7962c870cc297fa230ea1459
Ketan Sharma <ketan10923@gmail.com>
2024-11-21 03:56:12 +0100

    Author and Sources added

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
-
 import Spinner from "./Spinner";
 
 const NEWS_API_KEY = import.meta.env.VITE_API_KEY;
@@ -29,6 +28,7 @@ export class News extends Component {
   };
 
   async componentDidMount() {
+    this.updateDocumentTitle();
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=${NEWS_API_KEY}&page=1&pageSize=${this.state.pageSize}`;
     if (this.props.category) {
       url += `&category=${this.props.category}`;
@@ -42,6 +42,20 @@ export class News extends Component {
       loading: false,
     });
   }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.category !== this.props.category) {
+      this.updateDocumentTitle();
+    }
+  }
+
+  updateDocumentTitle = () => {
+    if (this.props.category) {
+      document.title = `News Jockey - ${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}`;
+    } else {
+      document.title = "News Jockey";
+    }
+  };
 
   handlePrevClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=${NEWS_API_KEY}&page=${this.state.page - 1}&pageSize=${this.state.pageSize}`;
@@ -83,47 +97,45 @@ export class News extends Component {
   render() {
     return (
       <div
-      /* style={{
-        backgroundImage: `url(${BgOne})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-      }} */
-      className="pt-6 bg-[#f5f5f5]"
+        className="pt-6 bg-[#f5f5f5]"
       >
-      <h2 className="text-3xl p-5 text-center bg-gradient-to-r from-[#3498db] to-[#2ecc71] mb-6">
-         {this.props.category ? `Top Headlines in ${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}` : 'Top Headlines'}
-      </h2>
-      {this.state.loading && <Spinner />}
-      {!this.state.loading && (
-        <div className="columns-1 sm:columns-3 gap-4 space-y-20 sm:mt-20 sm:w-[85vw] m-auto">
-        {this.state.articles.map((element) => (
-          <div className="break-inside-avoid m-auto" key={element.url}>
-          <NewsItem
-            title={element.title ? element.title : ""}
-            desc={element.description ? element.description : ""}
-            img={element.urlToImage ? element.urlToImage : ""}
-            newsUrl={element.url ? element.url : ""}
-          />
+        <h2 className="text-4xl font-serif p-5 text-center bg-gradient-to-r from-[#3498db] to-[#2ecc71] mb-6 shadow-lg rounded-lg border-b-4 border-[#2ecc71] text-white">
+          {this.props.category ? `Top Headlines in ${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)}` : 'Top Headlines'}
+        </h2>
+        {this.state.loading && <Spinner />}
+        {!this.state.loading && (
+          <div className="columns-1 sm:columns-3 gap-4 space-y-20 sm:mt-20 sm:w-[85vw] m-auto">
+            {this.state.articles.map((element) => (
+              <div className="break-inside-avoid m-auto" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  desc={element.description ? element.description : ""}
+                  img={element.urlToImage ? element.urlToImage : ""}
+                  newsUrl={element.url ? element.url : ""}
+                  source={element.source.name ? element.source.name : "Unknown"}
+                  author={element.author ? element.author : "Unknown"}
+                  date={element.publishedAt ? element.publishedAt : "Unknown Date"}
+                />
+              </div>
+            ))}
           </div>
-        ))}
+        )}
+        <div className="w-[80vw] flex justify-between m-auto mt-8">
+          <button
+            disabled={this.state.page <= 1}
+            className="text-white bg-black p-4 rounded-2xl"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pageSize)}
+            className="text-white bg-black p-4 rounded-2xl"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
-      )}
-      <div className="w-[80vw] flex justify-between m-auto mt-8">
-        <button
-        disabled={this.state.page <= 1}
-        className="text-white bg-black p-4 rounded-2xl"
-        onClick={this.handlePrevClick}
-        >
-        &larr; Previous
-        </button>
-        <button
-        disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.state.pageSize)}
-        className="text-white bg-black p-4 rounded-2xl"
-        onClick={this.handleNextClick}
-        >
-        Next &rarr;
-        </button>
-      </div>
       </div>
     );
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,65 +1,75 @@
-import React, { Component } from "react";
-import PropTypes from 'prop-types';
+  import React, { Component } from "react";
+  import PropTypes from 'prop-types';
 
-export class NewsItem extends Component {
-  render() {
-    let { title, desc, img, newsUrl } = this.props;
-    const truncatedDesc = desc.length > 71 ? desc.substring(0, 105) + "..." : desc;
-    const truncatedTitle = title.length > 55 ? title.substring(0, 55) + "..." : title;
+  export class NewsItem extends Component {
+    render() {
+      let { title, desc, img, newsUrl, source, author, date } = this.props;
+      const truncatedDesc = desc.length > 71 ? desc.substring(0, 105) + "..." : desc;
+      const truncatedTitle = title.length > 55 ? title.substring(0, 55) + "..." : title;
 
-    return (
-      <div className="flex justify-center">
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-700">
-          <a href={newsUrl} target="_blank">
-            <img
-              className="rounded-t-lg"
-              src={img}
-              alt=""
-            />
-          </a>
-          <div className="p-5">
+      return (
+        <div className="flex justify-center">
+          <div className="relative max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-700">
             <a href={newsUrl} target="_blank">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-[#333333] line-clamp-2">
-                {title}
-              </h5>
+              <img
+                className="rounded-t-lg"
+                src={img}
+                alt=""
+              />
+              <div className="absolute top-2 right-2 bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-red-500 dark:text-white z-10">
+               {source}
+              </div>
             </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-[#757575] line-clamp-2" >
-              {desc}
-            </p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#3498db] dark:hover:bg-[#2980b9] dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+            <div className="p-5">
+              <a href={newsUrl} target="_blank">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-[#333333] line-clamp-2">
+                  {title}
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-[#757575] line-clamp-2 text-base" >
+                {desc}
+              </p>
+              
+              <a
+                href={newsUrl}
+                target="_blank"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-[#3498db] dark:hover:bg-[#2980b9] dark:focus:ring-blue-800 mt-2"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
+                Read more
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+              <p className="text-sm text-gray-500 dark:text-gray-400 mt-3">
+                Published by {author} on {new Date(date).toGMTString()}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
-NewsItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  desc: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
-  newsUrl: PropTypes.string.isRequired,
-};
+  NewsItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    desc: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    newsUrl: PropTypes.string.isRequired,
+    source: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  };
 
-export default NewsItem;
+  export default NewsItem;
